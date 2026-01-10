15c2ff0bdbed0d4954b6216d070285adc416c403
Sheng Ang <70978946+ThongSheng@users.noreply.github.com>
2026-01-10 14:23:04 -0500

    Add files via upload

--- final/4.4 comparison_function.R
+++ final/4.4 comparison_function.R
@@ -7,8 +7,8 @@ grid <- expand.grid(
   'p' = c(2, 10, 20),
   'n' = c(20, 100, 150),
   'seed' = 1:30,
-  'func' = c('determ_full', 'determ_2d', 'GP_full', 'GP_2d'),
-  'prior_choice' = c('bass', 'wycoff', 'mle')
+  'func' = c('Full deterministic', '2d deterministic', 'Full GP', '2d GP'),
+  'prior_choice' = c('BASS', 'GP Conditional', 'GP MLE')
 )
 
 output_dir <- '/scratch/negishi/angt/nonstan_results/' # change this to match
@@ -49,8 +49,8 @@ for (i in 1:length(files)) {
     d = grid$p[file_id],
     n = grid$n[file_id],
     seed = grid$seed[file_id],
-    func = grid$func[file_id],
-    method = grid$prior_choice[file_id],
+    Function = grid$func[file_id],
+    Method = grid$prior_choice[file_id],
     time_used = as.double(time_used),
     frobenius = frobenius,
     cos_sim = cos_sim,
@@ -62,14 +62,13 @@ for (i in 1:length(files)) {
 # Combine the list into a single data frame
 nonstan_results_df <- do.call(rbind, nonstan_results_list)
 
-
 # --- STAN ---
 grid <- expand.grid(
   'p' = c(2, 10, 20),
   'n' = c(20, 100, 150),
   'seed' = 1:30,
-  'func' = c('determ_full', 'determ_2d', 'GP_full', 'GP_2d'),
-  'prior_choice' = c("dirichlet_wishart", "gp_reduce", "lognormal_inverse_wishart")
+  'func' = c('Full deterministic', '2d deterministic', 'Full GP', '2d GP'),
+  'prior_choice' = c("Dirichlet-Wishart", "GP Reduce", "Log-normal Inverse-Wishart")
 )
 
 output_dir <- '/scratch/negishi/angt/stan_results/' # change this to match
@@ -78,7 +77,8 @@ file_ids <- as.integer(gsub(".RData$", "", gsub(".*_", "", files)))
 
 stan_results_list <- list()
 
-for (i in 1:length(files)) {
+#for (i in 1:length(files)) {
+for (i in 3001:3240) {
   file_id <- file_ids[i]
   
   # Load the file
@@ -114,8 +114,8 @@ for (i in 1:length(files)) {
     d = grid$p[file_id],
     n = grid$n[file_id],
     seed = grid$seed[file_id],
-    func = grid$func[file_id],
-    method = grid$prior_choice[file_id],
+    Function = grid$func[file_id],
+    Method = grid$prior_choice[file_id],
     time_used = as.double(time_used),
     frobenius = frobenius,
     cos_sim = cos_sim,
@@ -130,24 +130,36 @@ stan_results_df <- do.call(rbind, stan_results_list)
 # --- Combine both STAN and NONSTAN ---
 results_df <- rbind(nonstan_results_df, stan_results_df)
 
+# Define colors
+method_colors <- c(
+  "BASS"                       = "#F8766D",
+  "GP Conditional"             = "#B79F00",
+  "GP MLE"                     = "#00BA38",
+  "Dirichlet-Wishart"          = "#00BFC4",
+  "GP Reduce"                  = "#619CFF",
+  "Log-normal Inverse-Wishart" = "#F564E3"
+)
+
 # Plot 1: Log of Computation Time
 (log_time <- ggplot(data = results_df) +
-  geom_boxplot(aes(x = factor(n), y = log(time_used), color = method)) +
-  facet_grid(d ~ func, labeller = label_both) +
+  geom_boxplot(aes(x = factor(n), y = log(time_used), color = Method)) +
+  facet_grid(d ~ Function, labeller = label_both) +
   labs(x = 'Sample size (n)', 
        y = 'Log of computation time (minutes)') +
   theme_bw() +
-  theme(text = element_text(family = 'Arial')))
+  theme(text = element_text(family = 'Arial'))) +
+  scale_color_manual(values = method_colors)
 ggsave("Desktop/log_time.png", plot = log_time, width = 10, height = 5, units = "in", dpi = 300)
 
 # Plot 2: Frobenius Norm between normalized C and C_est
 (frob <- ggplot(data = results_df) +
-  geom_boxplot(aes(x = factor(n), y = frobenius, color = method)) +
-  facet_grid(d ~ func, labeller = label_both) +
+  geom_boxplot(aes(x = factor(n), y = frobenius, color = Method)) +
+  facet_grid(d ~ Function, labeller = label_both) +
   labs(x = 'Sample size (n)', 
        y = 'Frobenius Norm') +
   theme_bw() +
-  theme(text = element_text(family = 'Arial')))
+  theme(text = element_text(family = 'Arial'))) +
+  scale_color_manual(values = method_colors)
 ggsave("Desktop/frob.png", plot = frob, width = 10, height = 5, units = "in", dpi = 300)
 
 # Plot 3: Cosine Similarity between first eigenvectors
@@ -157,33 +169,36 @@ line_data <- data.frame(
 )
 
 (cosine <- ggplot(data = results_df) +
-  geom_boxplot(aes(x = factor(n), y = cos_sim, color = method)) +
+  geom_boxplot(aes(x = factor(n), y = cos_sim, color = Method)) +
   geom_hline(data = line_data, aes(yintercept = yintercept), color = "red", linetype = "dashed") +
-  facet_grid(d ~ func, labeller = label_both) +
+  facet_grid(d ~ Function, labeller = label_both) +
   labs(x = 'Sample size (n)', 
        y = 'Cosine Similarity of First Eigenvectors') +
   theme_bw() +
-  theme(text = element_text(family = 'Arial')))
+  theme(text = element_text(family = 'Arial'))) +
+  scale_color_manual(values = method_colors)
 ggsave("Desktop/cosine.png", plot = cosine, width = 10, height = 5, units = "in", dpi = 300)
 
 # Plot 4: Differences in first eigenvalue
 (first_eigen <- ggplot(data = results_df) +
-  geom_boxplot(aes(x = factor(n), y = diff_eigenvalue, color = method)) +
-  facet_grid(d ~ func, labeller = label_both) +
+  geom_boxplot(aes(x = factor(n), y = diff_eigenvalue, color = Method)) +
+  facet_grid(d ~ Function, labeller = label_both) +
   labs(x = 'Sample size (n)', 
        y = 'Abs Difference in First Eigenvalues') +
   theme_bw() +
-  theme(text = element_text(family = 'Arial')))
+  theme(text = element_text(family = 'Arial'))) +
+  scale_color_manual(values = method_colors)
 ggsave("Desktop/first_eigen.png", plot = first_eigen, width = 10, height = 5, units = "in", dpi = 300)
 
 # Plot 5: Effective sample size
 (ess <- ggplot(data = results_df) +
-    geom_boxplot(aes(x = factor(n), y = n_eff, color = method)) +
-    facet_grid(d ~ func, labeller = label_both) +
-    labs(x = 'Sample size (n)', 
-         y = 'Effective sample size (out of 4000)') +
-    ylim(0, 4000) +
-    theme_bw() +
-    theme(text = element_text(family = 'Arial')))
+  geom_boxplot(aes(x = factor(n), y = n_eff, color = Method)) +
+  facet_grid(d ~ Function, labeller = label_both) +
+  labs(x = 'Sample size (n)', 
+       y = 'Effective sample size (out of 4000)') +
+  ylim(0, 4000) +
+  theme_bw() +
+  theme(text = element_text(family = 'Arial')) +
+  scale_color_manual(values = method_colors))
 ggsave("Desktop/ess.png", plot = ess, width = 10, height = 5, units = "in", dpi = 300)
 
